dc5714f6f40c90b9940421e63a3ef144cbac6d51
boernerr <rboerner363@gmail.com>
2022-06-04 15:32:38 -0400

    Add dummy.py to simulate imports from /src

--- clipkg/__main__.py
+++ clipkg/__main__.py
@@ -6,6 +6,7 @@ import sys
 # Absolute import:
 from clipkg import hello, another_function
 from clipkg.moduleA import hello_moduleA, dummy_run_func
+from clipkg.src import dummy
 # from clipkg.src import gluestick
 # Relative import:
 # from . import hello, another_function
@@ -16,12 +17,15 @@ def main():
     parser.add_argument('-r', '--run', action='store_true', help='the argument value should =="run" ')
     parser.add_argument('-tf', '--transform', action='store_true')
     parser.add_argument('-pr', '--predict', action='store_true')
+    parser.add_argument('-dy', '--dummy', action='store_true')
     args = parser.parse_args()
     print(f'args supplied: {args}')
     # args.__arg_name__ is a string variable:
 
     if args.run:
         dummy_run_func()
+    if args.dummy:
+        dummy.src_dummy_func()
     # if args.transform:
     #     gluestick.mk_data()
     # if args.predict:
---
+++ clipkg/src/dummy.py
@@ -0,0 +1,2 @@
+def src_dummy_func():
+    print(f'this simulates running a func from a module at file {__file__}')
